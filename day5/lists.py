faa65118b7c96cadeab84775703479db76e8f1e7
carlapdl <macarla.poncedeleon@gmail.com>
2025-09-26 15:20:46 +0800

    included additional exercises for Day5

--- day5/lists.py
+++ day5/lists.py
@@ -115,7 +115,7 @@ del it_companies
 front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 back_end = ['Node','Express', 'MongoDB']
 frontend_backend = front_end + back_end
-print(frontend_backend)
+#print(frontend_backend)
 
 #After joining the lists (front_end, back_end), copy the joined list and assign it to a variable full_stack, 
 #then insert Python and SQL after Redux.
@@ -124,4 +124,59 @@ redux_index = full_stack.index('Redux')
 full_stack.insert(redux_index+1, 'Python')
 python_index = full_stack.index('Python')
 full_stack.insert(python_index+1, 'SQL')
-print("Full Stack: ", full_stack)
+#print("Full Stack: ", full_stack)
+
+#Exercise Level 2
+
+#The following is a list of 10 students ages:
+#* Sort the list and find the min and max age
+#* Add the min age and the max age again to the list
+#* Find the median age (one middle item or two middle items divided by two)
+#* Find the average age (sum of all items divided by their number )
+#* Find the range of the ages (max minus min)
+#* Compare the value of (min - average) and (max - average), use abs() method
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+sorted_ages = sorted(ages)
+print(f"Sorted Age List: {sorted_ages}")
+
+min_age = min(sorted_ages)
+max_age = max(sorted_ages)
+print(f"Minimum Age: {min_age}")
+print(f"Maximum Age: {max_age}")
+
+total_items = len(sorted_ages)
+print(f"Total items: {total_items}")
+
+mid_age_item_index = round((total_items - 1) / 2)
+#print("Mid Age Index: ", mid_age_item_index)
+mid_age_item = sorted_ages[mid_age_item_index]
+print(f"Median Age: {mid_age_item}")
+
+total_age = 0
+for age_item in sorted_ages:
+    total_age = total_age + age_item
+
+#print("Total Age: ", total_age)
+ave_age = round(total_age / total_items)
+print(f"Average Age: {ave_age}")
+print(f"Age Ranges: {max_age - min_age}")
+print(f"Abs Min. Average: {abs(min_age - ave_age)}")
+print(f"Abs Max. Average: {abs(max_age - ave_age)}")
+
+countries_list = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+
+print(f"Countries: {countries_list}")
+#Find the middle country(ies)
+total_country_items = len(countries_list)
+last_country_index = total_country_items - 1
+mid_country_index = round(last_country_index / 2)
+print(f"Mid Country: {countries_list[mid_country_index]}")
+
+#Divide the countries list into two equal lists if it is even if not one more country for the first half.
+print(f"First Half Country: {countries_list[0:(mid_country_index+1)]}")
+print(f"Next Half Countries: {countries_list[(mid_country_index+1):total_country_items]}")
+
+#Unpack the first three countries and the rest as scandic countries.
+print(f"First 3 Countries: {countries_list[0:3]}")
+print(f"Scandic Countries: {countries_list[(-mid_country_index):]}")
