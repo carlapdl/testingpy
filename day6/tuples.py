5cde88db0ea5ee2742ca5339a28ab8a2831f5230
carlapdl <macarla.poncedeleon@gmail.com>
2026-01-16 10:51:26 +0800

    additional exercises for day6

--- day6/tuples.py
+++ day6/tuples.py
@@ -11,14 +11,38 @@ brothers = ('Natsu', 'Gray', 'Leon')
 
 #Join brothers and sisters tuples and assign it to siblings
 siblings = sisters + brothers
-print(siblings)
+#print(siblings)
 
 #How many siblings do you have?
-print(len(siblings))
+#print(len(siblings))
 
 #Modify the siblings tuple and add the name of your father and mother and assign it to family_members
-siblings = list(siblings)
-siblings[0] = 'Makarov'
-siblings[1] = 'Nonoka'
-siblings = tuple(siblings)
-print(siblings)
+parents = ('Jonathan', 'Nancy')
+family_members = siblings + parents
+#print(family_members)
+
+#Exercises: Level 2
+
+#Create fruits, vegetables and animal products tuples. 
+#Join the three tuples and assign it to a variable called food_stuff_tp.
+fruits = ('saging', 'bayabas', 'lansones', 'mansanas')
+veggies = ('sitaw', 'bataw', 'patani', 'labanos', 'mustasa')
+animal_products = ('gatas', 'keso', 'itlog', 'mantikilya', 'mantika')
+food_stuff_tp = fruits + veggies + animal_products
+print(food_stuff_tp)
+
+#Change the food_stuff_tp tuple to a food_stuff_lt list
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
+
+#Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+food_lst_len = len(food_stuff_lt)
+print(food_lst_len)
+food_lst_mid = round(food_lst_len / 2)
+print(food_lst_mid)
+food_mid_item = food_stuff_lt[food_lst_mid]
+print(food_mid_item)
+food_first_three = food_stuff_lt[0:3]
+print(food_first_three)
+food_last_three = food_stuff_lt[-3:]
+print(food_last_three)
